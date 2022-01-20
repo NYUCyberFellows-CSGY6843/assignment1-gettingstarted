b9478f8e4105e915b47485b8463abe10aab38bb6
Rockstaru <slupeasaurus@gmail.com>
2022-01-19 19:26:33 -0500

    Initial commit

---
+++ solution.py
@@ -0,0 +1,30 @@
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
+
+def welcome_assignment_answers(question):
+    #Students do not have to follow the skeleton for this assignment.
+    #Another way to implement is using a "case" statements similar to C.
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "The student should type the answer here"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "The student should type the answer here"
+    return(answer)
+# Complete all the questions.
+
+
+if __name__ == "__main__":
+    #use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+
+###Questions:
+###"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
+###"Are encoding and encryption the same? - Yes/No"
+###"Is it possible to decrypt a message without a key? - Yes/No"
+###"Is it possible to decode a message without a key? - Yes/No"
+###"Is a hashed message supposed to be un-hashed? - Yes/No"
+###"What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
+###"Is MD5 a secured hashing algorithm? - Yes/No"
+###"What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
+###"What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
