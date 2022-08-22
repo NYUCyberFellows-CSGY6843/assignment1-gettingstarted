4c2d8f20b35f864bdfbc21115f042cde66aedbb6
ca1878 <72635836+ca1878@users.noreply.github.com>
2022-08-22 12:00:10 -0400

    Updated questions

--- solution.py
+++ solution.py
@@ -12,7 +12,7 @@ def welcome_assignment_answers(question):
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
-        answer = "" 
+        answer = ""
     return(answer)
 # Complete all the questions.
 
@@ -22,13 +22,13 @@ if __name__ == "__main__":
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
 
-###Questions:
-###"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
-###"Are encoding and encryption the same? - Yes/No"
-###"Is it possible to decrypt a message without a key? - Yes/No"
-###"Is it possible to decode a message without a key? - Yes/No"
-###"Is a hashed message supposed to be un-hashed? - Yes/No"
-###"What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code"
-###"Is MD5 a secured hashing algorithm? - Yes/No"
-###"What layer from the TCP/IP model does the protocol DHCP belong to? - The answer should be an integer number"
-###"What layer of the TCP/IP model does the protocol TCP belong to? - The answer should be an integer number"
+#Questions:
+#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+#"Are encoding and encryption the same? - Yes/No":
+#"Is it possible to decrypt a message without a key? - Yes/No":
+#"Is it possible to decode a message without a key? - Yes/No":
+#"Is a hashed message supposed to be un-hashed? - Yes/No":
+#"What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
+#"Is MD5 a secured hashing algorithm? - Yes/No":
+#"What layer of the OSI model does the protocol DHCP belong to? - The answer should be an integer number":
+#"What layer of the OSI model does the protocol TCP belong to? - The answer should be an integer number":
