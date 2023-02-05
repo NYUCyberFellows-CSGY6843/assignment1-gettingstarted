2edf50825726313a24c0fa239c15993c75463982
aabdikadir <124479410+aabdikadir@users.noreply.github.com>
2023-02-04 22:27:34 -0600

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -25,7 +25,7 @@ def welcome_assignment_answers(question):
         answer = 3
     else:
         answer = "Wrong answer"
-    else: 
+        
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue
