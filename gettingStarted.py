b8e94bf64605e05b526d5584959bee8101512e97
aabdikadir <124479410+aabdikadir@users.noreply.github.com>
2023-02-04 22:53:02 -0600

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -20,7 +20,8 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 7
+        answer = 4
+       
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
     else:
