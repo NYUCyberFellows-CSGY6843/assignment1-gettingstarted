8095a0978f0e3021c4355e1dfe6bfb12ecb581a0
Jen Smith <js12020@nyu.edu>
2023-08-11 19:26:41 -0700

    Updates for Fall 2023

--- gettingStarted.py
+++ gettingStarted.py
@@ -13,7 +13,7 @@ def welcome_assignment_answers(question):
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
-        answer = "Why am i here?"
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
     return(answer)
 # Complete all the questions.
 
