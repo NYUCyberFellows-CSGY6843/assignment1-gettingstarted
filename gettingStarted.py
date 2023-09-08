c10a4272146340512e193f8614d84dc834ac28a1
Kyle Bethel <kbethel@drew.edu>
2023-09-08 16:37:32 -0400

    testing git/pycharm

--- gettingStarted.py
+++ gettingStarted.py
@@ -4,28 +4,26 @@
 
 def welcome_assignment_answers(question):
 
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code -":
-        answer = "No"
-    elif "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No Idea"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "3"
+        answer = "1"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "3"
+        answer = "1"
     else:
-        answer = "an error occured"
+        answer = "An Error Occured. Please Try and Resubbitt Again"
     return(answer)
 
 
