600d0abb163b735fcc85f8b1dfda14fb6432f1ab
Kyle Bethel <kbethel@drew.edu>
2023-09-08 16:44:39 -0400

    1, 8, 9 failing

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,11 +1,11 @@
-### welcome_assignment_answers
-### Input - All nine questions given in the assignment.
-### Output - The right answer for the specific question.
+
+##testing answers
+
 
 def welcome_assignment_answers(question):
 
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer = "pcap"
+        answer = 'pcap'
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
@@ -23,12 +23,11 @@ def welcome_assignment_answers(question):
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = "4"
     else:
-        answer = "An Error Occured. Please Try and Resubbitt Again"
-    return(answer)
+        answer = "An Error occurred. Please try and resubmit again"
+    return answer
 
 
 if __name__ == "__main__":
-    #use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
     debug_answer = "No"
     print(welcome_assignment_answers(debug_question))
