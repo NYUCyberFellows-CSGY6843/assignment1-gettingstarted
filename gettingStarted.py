deb7c98ceb3f7d632d5de01063311c053e18526f
Kyle Bethel <kbethel@drew.edu>
2023-09-08 16:38:51 -0400

    testing git/pycharm

--- gettingStarted.py
+++ gettingStarted.py
@@ -11,7 +11,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code -":
@@ -19,9 +19,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "1"
+        answer = "2"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "1"
+        answer = "2"
     else:
         answer = "An Error Occured. Please Try and Resubbitt Again"
     return(answer)
