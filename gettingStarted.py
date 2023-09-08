385b6dcf5655dd1f50d3dd88aa991408f43bdbc4
Kyle Bethel <kbethel@drew.edu>
2023-09-08 17:01:02 -0400

    testing one wrong

--- gettingStarted.py
+++ gettingStarted.py
@@ -23,7 +23,7 @@ def welcome_assignment_answers(question):
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
     else:
-        answer = "No"
+        answer = "testing"
     return answer
 
 
