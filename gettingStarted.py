75bae31d1038cb5fca5beae0dcdb2d053dba764e
Kyle Bethel <kbethel@drew.edu>
2023-09-08 16:50:19 -0400

    1, 8, 9 failing

--- gettingStarted.py
+++ gettingStarted.py
@@ -19,9 +19,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "5"
+        answer = "6"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "5"
+        answer = "3"
     else:
         answer = "pcap"
     return answer
